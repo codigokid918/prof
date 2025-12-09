ee42b1ded52ea5985251d9db9804428c265dce89
codigokid918 <codigokid.918@gmail.com>
2025-12-09 17:34:48 -0300

    aula03

--- ANGULAR/lista-compra/src/app/mercadorias/mercadorias.ts
+++ ANGULAR/lista-compra/src/app/mercadorias/mercadorias.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './mercadorias.css',
 })
 export class Mercadorias {
-
+  mercadorias = [
+    {
+      descricao: 'Arroz',
+      quantidade: 1
+    },
+    {
+      descricao:"Feijão",
+      quantidade:5
+    },
+    {
+      descricao:"Óleo",
+      quantidade: 2
+    }
+]
 }
