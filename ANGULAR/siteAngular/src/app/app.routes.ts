b46b20e79369696b02e2eab719c18f543432631d
Código Kid <codigokid.918@gmail.com>
2026-01-13 17:52:42 -0300

    Criando componente home

---
+++ ANGULAR/siteAngular/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [];
+//Controla as rotas que a nossa apliação vai ter (Ex.: Home, Loja, Perfil, etc.)
---
+++ ANGULAR/siteAngular/src/app/app.ts
@@ -0,0 +1,19 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Home } from './components/home/home';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, Home],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+
+export class App {
+  title: string;
+
+  constructor() {
+    this.title = "Teste"
+  }
+}
