d8161b504fe4020a8f53e891a413eb035640e9a6
Código Kid Indaiatuba <codigokid.918@gmail.com>
2026-01-16 17:01:41 -0300

    Ajustando rotas

--- ANGULAR/siteAngular/src/app/app.ts
+++ ANGULAR/siteAngular/src/app/app.ts
@@ -1,19 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Home } from './components/home/home';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Home],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
 export class App {
-  title: string;
-
-  constructor() {
-    this.title = "Teste"
-  }
 }
